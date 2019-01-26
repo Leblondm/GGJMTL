c87adf946d03adf350554b61dced1d89f1935400
Marcel Leblond <leblondmarcel@videotron.ca>
2019-01-26 08:36:47 -0500

    merge conflict correction

---
+++ GGJMTL2019/Assets/Scripts/HUDController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Debug.Log(GameManager.Instance.isNormalWorld);
+        GameObject.FindGameObjectWithTag("DarkWorldTimerBar").GetComponent<SpriteRenderer>().enabled = !GameManager.Instance.isNormalWorld;
+    }
+}
