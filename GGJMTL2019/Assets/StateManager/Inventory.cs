7d07924085b91d6e4be93371265e8cbb48719b27
manuel.payet <manuel.payet@cgi.com>
2019-01-26 08:39:49 -0500

    Audio switch with world switching

---
+++ GGJMTL2019/Assets/StateManager/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+
+    public enum ItemTypes {
+        SCISSOR
+    }
+    private Dictionary<ItemTypes, int> inventory = new Dictionary<ItemTypes, int>();
+
+
+    public bool hasItem(ItemTypes item) {
+        if (!inventory.ContainsKey(item)) {
+            return false;
+        }
+
+        return inventory[item] > 0;
+    }
+}
