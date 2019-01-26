d7730f2a226076337ef9250aec96b1620664ccaa
manuel.payet <manuel.payet@cgi.com>
2019-01-26 02:31:08 -0500

    Première partie du GameManager

---
+++ GGJMTL2019/Assets/Scripts/WorldSwitcher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSwitcher : MonoBehaviour
+{
+
+    public GameManager gameManager;
+    public GameObject otherWorldMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("World switch"))
+        {
+            StartCoroutine(goToOtherWorld());
+        }
+    }
+
+    
+    public IEnumerator goToOtherWorld() {
+        if(GameManager.Instance.isNormalWorld)
+        {
+            Debug.Log("normal world: " + GameManager.Instance.isNormalWorld);
+            gameManager.isNormalWorld = false;
+            GameObject otherWorldInst = Instantiate(otherWorldMask);
+            yield return new WaitForSeconds(gameManager.otherWorldTimeout);
+            Destroy(otherWorldInst);
+            gameManager.isNormalWorld = true;
+        }
+        yield return null;
+    }
+}
---
+++ GGJMTL2019/Assets/StateManager/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (GameManager.instance == null)
+            {
+                GameManager.instance = new GameManager();
+              //  DontDestroyOnLoad(GameManager.instance);
+            }
+            return GameManager.instance;
+        }
+
+
+    }
+    public bool isNormalWorld = true;
+    public float otherWorldTimeout = 3f;
+}
